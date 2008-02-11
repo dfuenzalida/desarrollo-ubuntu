1f905d524a200845cf51abae489ef944ec204a63
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2008-02-11 19:00:06 +0000

    Agregado mini-ejemplo Swing

---
+++ filesplitter-java/src/swing/SwingTest.java
@@ -0,0 +1,37 @@
+package swing;
+
+import java.awt.*;
+import java.util.*;
+import java.text.*;
+import javax.swing.*;
+
+public class SwingTest {
+    public static void main(String[] args) {
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("FileSplitter");
+        String message  = resourceBundle.getString("hello.world");
+        Object[] params = new Object[]{ new Date(), ""};
+        String greeting = MessageFormat.format(message, params);
+
+        JButton button = new JButton(greeting);
+        JFrame frame = new JFrame();
+
+        // Get default close operation
+        int op = frame.getDefaultCloseOperation(); // HIDE_ON_CLOSE
+
+        // Set to exit on close
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Add button to the frame
+        frame.getContentPane().add(button, BorderLayout.CENTER);
+
+        // Set initial size
+        int frameWidth = 300;
+        int frameHeight = 300;
+        frame.setSize(frameWidth, frameHeight);
+
+        // Show the frame
+        frame.setVisible(true);
+    }
+}
+
